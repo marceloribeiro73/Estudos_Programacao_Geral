d8277ade7e6b8b21344a604c933d6e6e5f23352e
Marcelo Henrique Ribeiro da Silva <marcelo.hrs08@gmail.com>
2023-08-04 22:59:26 -0300

    Nova forma no exercicio leap_year

--- Introdução_programacao/Praticas/exercism/04.08/leap_year.py
+++ Introdução_programacao/Praticas/exercism/04.08/leap_year.py
@@ -1,15 +1,19 @@
 #%%
 def leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
+    # if year % 4 == 0:
+    #     if year % 100 == 0:
+    #         if year % 400 == 0:
+    #             return True
+    #         else:
+    #             return False
+    #     else:
+    #         return True
+    # else:
+    #     return False
+    if (year % 4 == 0 and year % 100 == 0 and year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+        return True
     else:
         return False
 
 #%%
-leap_year(2000)
+leap_year(2015)
