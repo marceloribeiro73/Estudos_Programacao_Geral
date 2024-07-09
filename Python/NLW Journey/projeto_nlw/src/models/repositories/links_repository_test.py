3682b5eaa8143e1cd5de655d68536f5021381d84
Marcelo H. Ribeiro da Silva <marcelo.hrs08@gmail.com>
2024-07-09 20:29:55 -0300

    upd: aula.2 - update testes links

--- Python/NLW Journey/projeto_nlw/src/models/repositories/links_repository_test.py
+++ Python/NLW Journey/projeto_nlw/src/models/repositories/links_repository_test.py
@@ -7,6 +7,7 @@ db_connection_handler.connect()
 trip_id = str(uuid.uuid4())
 link_id = str(uuid.uuid4())
 
+@pytest.mark.skip(reason="interage com o banco de dados")
 def test_insert_link():
     conn = db_connection_handler.get_connection()
     links_repository = LinksRepository(conn)
@@ -19,12 +20,16 @@ def test_insert_link():
     }
 
     links_repository.insert_link(link_infos=link_infos)
-
+    
+@pytest.mark.skip(reason="interage com o banco de dados")
 def test_find_links_from_trip():
     conn = db_connection_handler.get_connection()
     links_repository = LinksRepository(conn)
 
     links = links_repository.find_links_from_trip(trip_id=trip_id)
 
-    print()
-    print(links)
+    #print()
+    #print(links)
+
+    assert isinstance(links, list)
+    assert isinstance(links[0], tuple)
