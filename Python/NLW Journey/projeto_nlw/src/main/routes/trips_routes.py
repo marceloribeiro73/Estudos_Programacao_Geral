912fee6acddecabd823ccbb75da78d5ab7add9b2
Marcelo H. Ribeiro da Silva <marcelo.hrs08@gmail.com>
2024-07-09 21:35:48 -0300

    add: adicionada rota para retonar trip

--- Python/NLW Journey/projeto_nlw/src/main/routes/trips_routes.py
+++ Python/NLW Journey/projeto_nlw/src/main/routes/trips_routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, Blueprint, request
 from src.controllers.trip_creator import TripCreator
+from src.controllers.trip_finder import TripFinder
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
 from src.models.settings.db_connection_handler import db_connection_handler
@@ -14,5 +15,15 @@ def create_trip():
     controller = TripCreator(trip_repository=trips_repository, emails_repository=emails_repository)
 
     response = controller.create(request.json)
+
+    return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<tripID>", methods=["GET"])
+def find_trip(tripID):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripFinder(trips_repository=trips_repository)
+
+    response = controller.find_trip_details(tripID)
 
     return jsonify(response["body"]), response["status_code"]
